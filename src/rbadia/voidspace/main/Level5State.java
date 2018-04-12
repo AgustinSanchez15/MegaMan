db9ec1c7d0e3b643e80806b7d3644fcce4af9338
Agustin Sanchez <agustin.sanchez@upr.edu>
2018-04-12 19:40:27 -0400

    Created level 5

---
+++ src/rbadia/voidspace/main/Level5State.java
@@ -0,0 +1,17 @@
+package rbadia.voidspace.main;
+
+import rbadia.voidspace.graphics.GraphicsManager;
+import rbadia.voidspace.sounds.SoundManager;
+
+public class Level5State extends Level2State{
+	private static final long serialVersionUID = -2094575762243216079L;
+	
+	// Constructors
+		public Level5State(int level, MainFrame frame, GameStatus status, 
+				LevelLogic gameLogic, InputHandler inputHandler,
+				GraphicsManager graphicsMan, SoundManager soundMan) {
+			super(level, frame, status, gameLogic, inputHandler, graphicsMan, soundMan);
+		}
+		
+		
+}
